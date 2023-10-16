8ba6b14901767a2496b2cdefd69b0a80b4b08968
Gabriel Souza <gabrielsfarias@outlook.com>
2023-10-16 05:24:56 -0300

    refactor(database): reorganize code and apply constants and error handling for database connection

--- src/shared/setupDatabaseAndContainer.js
+++ src/shared/setupDatabaseAndContainer.js
@@ -1,10 +1,11 @@
 const { CosmosClient } = require('@azure/cosmos')
 const { ConnectionFailedError } = require('../errors/connectionFailed.js')
+const { DATABASE } = require('../constants/database.js')
 
 const cosmosClient = new CosmosClient({
   endpoint: process.env.CosmosDB_Endpoint,
   key: process.env.CosmosDB_Key,
-  connectionPolicy: { requestTimeout: 90000, retryOptions: { maxRetryAttemptCount: 10 } }
+  connectionPolicy: { requestTimeout: 120000, retryOptions: { maxRetryAttemptCount: 10 } }
 })
 
 let database
@@ -14,23 +15,25 @@ async function initializeDatabase() {
   if (!container) {
     try {
       const dbResponse = await cosmosClient.databases.createIfNotExists({
-        id: 'concursos',
+        id: DATABASE.ID,
         throughput: 1000
       })
       database = dbResponse.database
 
       const containerResponse = await database.containers.createIfNotExists({
-        id: 'concursos',
-        partitionKey: { paths: ['/concurso'] },
-        uniqueKeyPolicy: { paths: ['/url'] }
+        id: DATABASE.CONTAINER_ID,
+        partitionKey: { paths: DATABASE.PARTITION_KEY_PATHS },
+        uniqueKeyPolicy: { paths: DATABASE.UNIQUE_KEY_PATHS }
       })
 
       container = containerResponse.container
     } catch (error) {
+      // Log the error for debugging purposes
+      console.error(`Error initializing database: ${error.message}`)
       throw new ConnectionFailedError()
     }
   }
   return container
 }
 
-exports.initializeDatabase = initializeDatabase
+module.exports = { initializeDatabase }
