be6a1f771f0ebb0e65ef97cd23a623376c2092ea
Gabriel Souza <gabrielsfarias@outlook.com>
2023-10-16 05:34:55 -0300

    fix(upsertitem.js): fix bug in the upsert operation in database
    Fixes a bug where upserting documents would duplicate and not verify for unique URLs

--- src/shared/upsertItem.js
+++ src/shared/upsertItem.js
@@ -24,6 +24,7 @@ async function upsertItem(container, request, $, scrapedData) {
       const arquivos = scrapedData.arquivos
 
       const item = {
+        id: arquivos[0].id, // Use the unique ID of the first file in the arquivos array
         banca: organizers.FCC,
         concurso,
         url: request.loadedUrl,
@@ -39,11 +40,42 @@ async function upsertItem(container, request, $, scrapedData) {
         throw new RequestTimeoutError()
       } else {
         log.error(error)
-        throw new UpsertError()
+        throw new UpsertError('Error upserting item to Cosmos DB: ' + error.message)
       }
     }
   } else {
-    log.info(`Item with URL ${request.loadedUrl} already exists, skipping`)
+    // An item with the same URL exists, check if data is different before upserting
+    const existingItem = existingItems[0] // Assuming only one document per URL
+
+    if (JSON.stringify(existingItem.arquivos) !== JSON.stringify(scrapedData.arquivos)) {
+      // Data is different, perform the upsert
+      try {
+        const item = {
+          id: existingItem.id, // Use the existing id
+          banca: organizers.FCC,
+          concurso: scrapedData.concurso,
+          url: request.loadedUrl,
+          arquivos: scrapedData.arquivos
+        }
+
+        await container.items.upsert(item)
+        log.info(`Upserting item: ${JSON.stringify(item)}`)
+        return scrapedData.arquivos // Return the updated data
+      } catch (error) {
+        if (error.code === 408) {
+          // Timeout error
+          throw new RequestTimeoutError()
+        } else {
+          log.error(error)
+          throw new UpsertError('Error upserting item to Cosmos DB: ' + error.message)
+        }
+      }
+    } else {
+      // Data is the same, skip the upsert
+      log.info(`Item with URL ${request.loadedUrl} already exists and data is the same, skipping`)
+      return existingItem.arquivos // Return the existing data
+    }
   }
 }
-exports.upsertItem = upsertItem
+
+module.exports = { upsertItem }
