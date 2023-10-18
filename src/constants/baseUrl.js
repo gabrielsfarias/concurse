498fa7ccba2b59cde0c8141b07d07608718ac57f
Gabriel Souza <gabrielsfarias@outlook.com>
2023-10-18 09:09:52 -0300

    fix(base_url): fixes the base url for cesgranrio

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,6 +1,6 @@
 module.exports.BASE_URL = {
   fcc: 'https://www.concursosfcc.com.br',
-  cesgranrio: 'https://www.cesgranrio.org.br/concursos/principal.aspx',
+  cesgranrio: 'https://www.cesgranrio.org.br/concursos',
   fgv: 'https://conhecimento.fgv.br/concursos',
   cebraspe: 'https://www.cebraspe.org.br/concursos/'
 }
