ee459bff8d1363d934ef623e5bd9359c5bbd41e9
Gabriel Souza <gabrielsfarias@outlook.com>
2023-10-20 00:55:39 -0300

    feat(fgv-scraper.js): implement basic scraping for FGV - WIP
    Basic scraping for FGV - missing files that are under other selectors other than the one specified
    under the handler

--- src/functions/fgv/fgv-scraper.js
+++ src/functions/fgv/fgv-scraper.js
@@ -1,8 +1,96 @@
 const { app } = require('@azure/functions')
+const { createCheerioRouter, CheerioCrawler, log } = require('crawlee')
+const { organizers } = require('../../constants/organizer.js')
+const { BASE_URL } = require('../../constants/baseUrl.js')
+const { ConnectionFailedError } = require('../../errors/connectionFailed.js')
+const { upsertItem } = require('../../shared/upsertItem.js')
+const { validateURL } = require('../../shared/validateURL.js')
+const { initializeDatabase } = require('../../shared/setupDatabaseAndContainer.js')
+const { getUniqueLinkId } = require('../../shared/uniqueId.js')
+const { SocketHangUpError } = require('../../errors/socketHangup.js')
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+let scraperFgv
+let scrapedData
+let container
 
 app.timer('FGV', {
-  schedule: '0 */5 * * * *',
-  handler: (myTimer, context) => {
+  schedule: '* */1 * * *',
+  runOnStartup: true,
+  handler: async (myTimer, context) => {
     context.log('Timer function processed request.')
+
+    try {
+      if (!container) {
+        container = await initializeDatabase() // Initialize the database if not already done
+      }
+
+      const startUrls = [`${BASE_URL.fgv}/concursos`]
+
+      const router = createCheerioRouter()
+      const banca = organizers.FGV
+
+      router.addDefaultHandler(async ({ enqueueLinks, log }) => {
+        log.info('enqueueing new URLs')
+        await enqueueLinks({
+          globs: [`${BASE_URL.fgv}/concursos/**`, `${BASE_URL.fgv}/sites/**`],
+          label: organizers.FGV
+        })
+      })
+
+      router.addHandler(banca, async ({ request, $, log }) => {
+        const concurso = $('div > div div > h1').text().trim()
+        const arquivos = $('.field__item > p a[href]')
+          .map((i, el) => {
+            const publicationText = $(el).text().trim()
+            const link = $(el).attr('href')
+            if (link.endsWith('.pdf') && validateURL(link)) {
+              // const urlSemIndexHtml = request.loadedUrl.replace('index.html', '')
+              const uniqueId = getUniqueLinkId(banca, concurso, link)
+              log.info(`concurso: ${concurso}publicationText: ${publicationText}`)
+              return { link, publicationText, uniqueId }
+            }
+            // console.log(concurso)
+            return null
+          })
+          .get()
+          .filter((item) => item !== null)
+        log.info(`${concurso}`, { url: request.loadedUrl })
+        log.info(`Arquivos: ${JSON.stringify(arquivos)}`)
+
+        scrapedData = {
+          banca,
+          concurso,
+          arquivos: arquivos.map((arquivo) => ({
+            id: arquivo.uniqueId,
+            publicationText: arquivo.publicationText.trim(),
+            link: arquivo.link
+          }))
+        }
+        upsertItem(container, request, $, scrapedData, banca) // Pass the data to upsertItem
+      })
+
+      scraperFgv = new CheerioCrawler({
+        additionalMimeTypes: ['application/pdf'],
+        requestHandler: router,
+        requestHandlerTimeoutSecs: 120,
+        retryOnBlocked: true,
+        maxRequestRetries: 5
+      })
+
+      await scraperFgv.run(startUrls)
+    } catch (error) {
+      if (error instanceof SocketHangUpError) throw new SocketHangUpError()
+      log.error(`Error in timer function: ${error.message}`)
+
+      throw new ConnectionFailedError(`Error in timer function: ${error.message}`)
+    }
   }
 })
+
+module.exports = {
+  scrapedData,
+  getSCraper: function () {
+    return scraperFgv
+  }
+}
